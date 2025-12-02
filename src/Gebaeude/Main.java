1578f69c0f18e20e734ad0c462be7124cfc58c29
Xipaya <magicbox@web.de>
2025-12-02 10:18:53 +0100

    Änderung einer Variablen

--- src/Gebaeude/Main.java
+++ src/Gebaeude/Main.java
@@ -3,7 +3,7 @@ package Gebaeude;
 public class Main {
     public static void main(String[] args) {
         int i = 5;
-        double d = 5.5;
+        double d = 10.5;
         boolean b = false;
 
 
